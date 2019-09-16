04ebf11fe47035da202003e2acc65a0578f9a9fd
kiddhustle <kiddhustle@wiardweb.com>
2019-09-16 13:44:06 +0100

    Updates

---
+++ precache-manifest.875a7362513e0e1a90af6694b8111ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac223e4d2b3b5d1c0fffa040f5e9afd",
+    "url": "/tests-exchange-rate-ui/index.html"
+  },
+  {
+    "revision": "ca6e6ab3f19a4277fd74",
+    "url": "/tests-exchange-rate-ui/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cae199d6eec2559346ff",
+    "url": "/tests-exchange-rate-ui/static/js/2.1fe621d3.chunk.js"
+  },
+  {
+    "revision": "ca6e6ab3f19a4277fd74",
+    "url": "/tests-exchange-rate-ui/static/js/main.91aec136.chunk.js"
+  },
+  {
+    "revision": "6a81ecfa601d346f50c4",
+    "url": "/tests-exchange-rate-ui/static/js/runtime~main.3d902da4.js"
+  }
+]);
